385529abdcf45e470d2675864c21bec0c7557864
raph2d68 <raphael.ausilio@epfl.ch>
2020-12-17 15:51:32 +0100

    Add files via upload

--- helper.py
+++ helper.py
@@ -3,66 +3,30 @@ import os
 import sys
 import urllib
 import matplotlib.image as mpimg
-from PIL import Image
-
 import code
-
+import matplotlib.pyplot as plt
 import tensorflow.python.platform
-
 import numpy as np
 import tensorflow as tf
 
+from PIL import Image
 from parameters import *
 
 
-def error_rate(predictions, labels):
-    """Return the error rate based on dense predictions and 1-hot labels."""
-    return 100.0 - (
-        100.0 *
-        np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1)) /
-        predictions.shape[0])
+#return an image data array (M, N, 3)
+def get_image(img):
+    return mpimg.imread(img)
 
-
-# Write predictions from neural network to a file
-def write_predictions_to_file(predictions, labels, filename):
-    max_labels = np.argmax(labels, 1)
-    max_predictions = np.argmax(predictions, 1)
-    file = open(filename, "w")
-    n = predictions.shape[0]
-    for i in range(0, n):
-        file.write(max_labels(i) + ' ' + max_predictions(i))
-    file.close()
-
-
-# Print predictions from neural network
-def print_predictions(predictions, labels):
-    max_labels = np.argmax(labels, 1)
-    max_predictions = np.argmax(predictions, 1)
-    print(str(max_labels) + ' ' + str(max_predictions))
-
-
-# Convert array of labels to an image
-def label_to_img(imgwidth, imgheight, w, h, labels):
-    array_labels = np.zeros([imgwidth, imgheight])
-    idx = 0
-    for i in range(0, imgheight, h):
-        for j in range(0, imgwidth, w):
-            if labels[idx][0] > 0.5:  # bgrd
-                l = 0
-            else:
-                l = 1
-            array_labels[j:j+w, i:i+h] = l
-            idx = idx + 1
-    return array_labels
-
-
-def img_float_to_uint8(img):
-    
+#Convert array data pixel, into value between 0-255 
+def img_float_to_uint8(img):    
     rimg = img - np.min(img)
     rimg = (rimg / np.max(rimg) * PIXEL_DEPTH).round().astype(np.uint8)
     return rimg
 
-
+# Return an image containing two images side by side
+# Input:        img         Input real image
+#               gt_img      Predicted groundtruth Image
+# Output:       cimg        concatenation of to images    
 def concatenate_images(img, gt_img):
     n_channels = len(gt_img.shape)
     w = gt_img.shape[0]
@@ -79,7 +43,10 @@ def concatenate_images(img, gt_img):
         cimg = np.concatenate((img8, gt_img_3c), axis=1)
     return cimg
 
-
+# Superimposes an input image and the label prediction for this image
+# Input:        img                 Input real image
+#               predicted_img       Predicted groundtruth Image
+# Output:       new_img             Superimposition of the two images
 def make_img_overlay(img, predicted_img):
     w = img.shape[0]
     h = img.shape[1]
@@ -92,36 +59,20 @@ def make_img_overlay(img, predicted_img):
     new_img = Image.blend(background, overlay, 0.2)
     return new_img
 
+# Compute the F1 score from the epoch history of the model, for testing set
+# Input:    epo_hist        contain Recall and Precision
+# Output:   f1_s            F1 score  
+def f1_scores(epo_hist):
+    pre = np.array(epo_hist.history['Precision'])
+    rec = np.array(epo_hist.history['Recall'])
+    f1_s = 2 * pre * rec / (pre + rec)
+    return f1_s
 
 
-def get_image_summary(img, idx=0):
-    V = tf.slice(img, (0, 0, 0, idx), (1, -1, -1, 1))
-    img_w = img.get_shape().as_list()[1]
-    img_h = img.get_shape().as_list()[2]
-    min_value = tf.reduce_min(input_tensor=V)
-    V = V - min_value
-    max_value = tf.reduce_max(input_tensor=V)
-    V = V / (max_value*PIXEL_DEPTH)
-    V = tf.reshape(V, (img_w, img_h, 1))
-    V = tf.transpose(a=V, perm=(2, 0, 1))
-    V = tf.reshape(V, (-1, img_w, img_h, 1))
-    return V
-
-# Make an image summary for 3d tensor image with index idx
-def get_image_summary_3d(img):
-    V = tf.slice(img, (0, 0, 0), (1, -1, -1))
-    img_w = img.get_shape().as_list()[1]
-    img_h = img.get_shape().as_list()[2]
-    V = tf.reshape(V, (img_w, img_h, 1))
-    V = tf.transpose(a=V, perm=(2, 0, 1))
-    V = tf.reshape(V, (-1, img_w, img_h, 1))
-    return V
-
-
-def value_to_class(v):
-    foreground_threshold = 0.25  # percentage of pixels > 1 required to assign a foreground label to a patch
-    df = np.sum(v)
-    if df > foreground_threshold:  # road
-        return [0, 1]
-    else:  # bgrd
-        return [1, 0]
+# plot the F1-score in function of epoch
+def plot_metric_history(f1_scores):
+    plt.plot(f1_scores)
+    plt.xlabel('# epochs')
+    plt.ylabel('F1-Score')
+    plt.title('F1-Score for every epochs')
+    plt.savefig('F1-Scores.png')
