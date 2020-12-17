b5d2252c0da4173dec621c771da0ed3b2cb3693a
raph2d68 <raphael.ausilio@epfl.ch>
2020-12-17 15:38:39 +0100

    Add files via upload
    "clean code"

--- post_processing.py
+++ post_processing.py
@@ -1,112 +1,88 @@
+import cv2
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from scipy import ndimage, misc
 
 from parameters import *
 from helper import *
 from pre_processing import *
-from model import model
-
-import matplotlib.pyplot as plt
-
-#opening followed by a closing
-#opening = erosion puis dilatation
-
-import cv2
-
-import numpy as np
-import tensorflow as tf
-from scipy import ndimage, misc
 
 
 
+# applies morphological operators on binary predicted image
+# input:    binary image [n, m]
+# output:   binary image [n, m]
 def morphological_op( binary_image ):
     if POST_PROCESS:
-        cv2.imwrite('1 binary input.jpg', binary_image*255)
         #define kernel dimension
         width=IMG_PATCH_SIZE
 
-        kernel_v = get_kernel(2*width, 0)
-        kernel_v2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(width, 3*width))
-        
-        kernel_h = get_kernel(2*width, 90 )
-        kernel_h2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3*width, width))
+        rho = 1                     #  resolution of the parameter r in pixels 
+        theta = np.pi/360          #  resolution of the parameter θ in radians
+        threshold = 100             #  minimum number of intersections to detect a line
+        minVal  = 5                 #  canny filter grad min and max treshold 
+        maxVal  = 100
+        #cv2.imwrite('4 sum of opening and closing.jpg',sum_image)
 
-        #vertical ligne
-        close_image_v  = cv2.morphologyEx(binary_image, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(width, width)))
-        cv2.imwrite('2 vertical_closing.jpg',close_image_v*255)
+        #Applies edges detection and hough transform on input image
+        binary_image = np.uint8((binary_image*255))
+        edges = cv2.Canny(binary_image, minVal, maxVal, apertureSize = 3)
         
-        opened_image_v = cv2.morphologyEx(close_image_v, cv2.MORPH_OPEN, get_kernel(3*width, 0))
-        cv2.imwrite('2 vertical_opening.jpg',opened_image_v*255)
-        
-        #horizontal_image
-        close_image_h  = cv2.morphologyEx(binary_image, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(width, width)))
-        cv2.imwrite('3 horizontal_closing.jpg',close_image_h*255)
-        
-        opened_image_h = cv2.morphologyEx(close_image_h, cv2.MORPH_OPEN, get_kernel(3*width, 90)) 
-        cv2.imwrite('3 horizontal_opening.jpg', opened_image_h*255)
-
-        sum_image = cv2.bitwise_or(opened_image_v, opened_image_h, mask = None) 
-        sum_image = np.uint8((sum_image*255))
-    
-        #rho	Distance resolution of the accumulator in pixels.
-        #theta	Angle resolution of the accumulator in radians.
-        #threshold	Accumulator threshold parameter. Only those lines are returned that get enough votes ( >threshold ).
-        rho = 1  
-        theta = 3.14/360  
-        threshold = 150
-        cv2.imwrite('4 sum of opening and closing.jpg',sum_image)
-    
-        edges = cv2.Canny(sum_image, 5,100,apertureSize = 3)
-        cv2.imwrite('5 edge_detection.jpg',edges)
-        line = cv2.HoughLines(edges, 1, np.pi/360,150)
+        line = cv2.HoughLines(edges, rho, theta, threshold)
         line = np.squeeze(line)
+        
         if np.size(line) > 1:
             #convert rad to degree
             b = line*360/(2*np.pi)
             if np.size(b)==2:
                 b=np.array([b])
             angles_means = get_mean_angle_values(b[:,1])
-            
-            #proper closing
-            #print("angle unique", angles_means)
-            idx_of_max_angle=find_max_values(angles_means, 2)
-            #print("idx_of_max_angle", idx_of_max_angle, 'type', type(idx_of_max_angle), "shape", np.shape(idx_of_max_angle) )
+            idx_of_max_angle = find_max_values(angles_means, NUMBER_OF_KERNEL)
             reduced_angles_means = [ angles_means[i] for i in idx_of_max_angle]
-            #print("reduced_unique_angle", reduced_angles_means)
-
+        
+            out_img = np.array(binary_image)/255
             for i, angle in enumerate(reduced_angles_means):
-                kernel_clean = get_kernel(3*width, angle)
-                out  = cv2.morphologyEx(sum_image, cv2.MORPH_CLOSE, kernel_clean)
-                #sum = cv2.bitwise_or(close_image_v, close_image_h, mask = None) 
-
-            cv2.imwrite('7 output.jpg',out)
-            return out/255
+                #opening and then closing for all different angles
+                out_op  = cv2.morphologyEx(binary_image, cv2.MORPH_OPEN, get_kernel(3*width, 2, angle))
+                out_cl  = cv2.morphologyEx(out_op, cv2.MORPH_CLOSE, get_kernel(5*width, 2, angle))
+                #summing all to input
+                out_img = cv2.bitwise_or(out_img, out_cl/255, mask = None) 
+            #round kernel to close   
+            round_ker = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(10, 10))
+            out_img  = cv2.morphologyEx(out_img, cv2.MORPH_CLOSE, round_ker)
+            return out_img/255
         else:
             print("no line hough detected")
-            return sum_image/255  
+            return binary_image/255  
         
     return binary_image
 
-
-def get_mean_angle_values( b ):
-
+# Average similar angles (of Hough Transform here) that are in a range tol  
+# intput: angle_list   1-D list of angle 
+# output: angles_mean  1-D list of mean of input angles
+def get_mean_angle_values( angle_list ):
     tol=TOL_ANGLE  
-    angles_mean=[]
+    angle_mean=[]
     i=0
-    full_len=len(b)
-    while len(b)>0 :  
-        smalltab=b[abs(b[0]-b)<tol]
+    full_len=len(angle_list)
+    while len(angle_list)>0 :  
+        smalltab=angle_list[abs(angle_list[0]-angle_list)<tol]
         if(len(smalltab)>(full_len/25)):
-            angles_mean.append(np.mean(smalltab))
-        b = b[abs(b[0]-b)>=tol]
+            angle_mean.append(np.mean(smalltab))
+        angle_list = angle_list[abs(angle_list[0]-angle_list)>=tol]
         i = i+1
-    
-    return angles_mean
+    return angle_mean
 
-def find_max_values(array, nb_of_max):
-    
+
+# Find index of "nb_of_max" maximum value in a 1-D array
+# Intput:   array       1-D array
+#           nb_of_max   Number of maximum value to extract from array
+# Output:   index       1-D array of size nb_of_max   
+def find_max_values(array, nb_of_max):  
     index=[]
     np_array = np.array(array)
-    new_np_array = np_array.copy()
-    
+    new_np_array = np_array.copy() 
     for i in range (nb_of_max):
         if len(new_np_array):
             max_value=np.max(new_np_array)
@@ -114,70 +90,48 @@ def find_max_values(array, nb_of_max):
             index.append(max_idx[0][0])
             mask=new_np_array!=np.max(new_np_array)
             new_np_array=new_np_array[mask]
-  
     return np.array(index)
 
 
-
-def get_kernel(width, angle):
-
-    ker = np.ones((width, 10 ), np.uint8)
+# Create a rectangular kernel for morphological operation, tilted of an certain angle
+# Intput:   width       Dimension of the kernel in pixel
+#           angle       Angle of tilting of kernel in degree
+# Output:   rot_ker     A titled kernel width*10 tilted by an angle "angle"
+def get_kernel(height , width,  angle):
+    ker = np.ones((height, width ), np.uint8)
     rot_ker = ndimage.rotate(ker, 180-angle, reshape=True)
-    #print(" kernel for angle", angle, rot_ker  )
-  
-    #cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(width+1, width+1))
-
     return rot_ker
 
 
-def generate_output(s, filename, image_idx, all_nodes): 
+# Generate output image with overlaying and concatenation of prediction
+# Intput:   image_idx       index of the current image
+#           img             current image                                   [n, m, 3 ]
+#           img_prediction  binary model prediction for current image       [n, m ]     
+def generate_output(image_idx, img, img_prediction ): 
     
-    imageid = "satImage_%.3d" % image_idx
-    image_filename = filename + imageid + ".png"
-    img = mpimg.imread(image_filename)
-  
-    print("IMG Idx", image_idx)
-
-    img_prediction = get_prediction(s, img, all_nodes)
-    img_prediction_processed = morphological_op(img_prediction)
+    print("IMG Idx prediction", image_idx)
+    dir_supp_images = prediction_training_dir + "visual/"
+    os.makedirs(dir_supp_images , exist_ok=True)
+    os.makedirs(prediction_training_dir, exist_ok=True)
+    
+    img_prediction_postprocessed = morphological_op(img_prediction)
     
     #with post_processing
-    pimg = get_prediction_with_groundtruth(img, img_prediction_processed, image_idx)
-    Image.fromarray(pimg).save(prediction_training_dir + "prediction_pp_" + str(image_idx) + ".png")
-    oimg = get_prediction_with_overlay(img, img_prediction_processed, image_idx)
-    oimg.save(prediction_training_dir + "overlay_pp_" + str(image_idx) + ".png")   
-    
-    #without  post_processing
-    pimg = get_prediction_with_groundtruth(img, img_prediction, image_idx)
-    Image.fromarray(pimg).save(prediction_training_dir + "prediction_" + str(image_idx) + ".png")
-    oimg = get_prediction_with_overlay(img, img_prediction, image_idx)
-    oimg.save(prediction_training_dir + "overlay_" + str(image_idx) + ".png")   
+    if POST_PROCESS:
+        Image.fromarray(img_float_to_uint8(img_prediction_postprocessed)).save(
+                                            prediction_training_dir + "prediction_pp_" + str(image_idx+1) + ".png")
+        pimg = concatenate_images(img, img_float_to_uint8(img_prediction_postprocessed))
+        Image.fromarray(pimg).save(dir_supp_images + "prediction_pp_" + str(image_idx+1) + ".png")
+        oimg = make_img_overlay(img, img_prediction_postprocessed)
+        #oimg.save(dir_supp_images + "overlay_pp_" + str(image_idx+1) + ".png")   
+    else : 
+        #without post_processing
+        Image.fromarray(img_float_to_uint8(img_prediction)).save(
+                                                prediction_training_dir + "prediction_" + str(image_idx+1) + ".png")
+        pimg = concatenate_images(img, img_prediction)
+        Image.fromarray(pimg).save(dir_supp_images + "prediction_" + str(image_idx+1) + ".png")
+        
+        oimg = make_img_overlay(img, img_prediction)
+        #oimg.save(dir_supp_images + "overlay_" + str(image_idx+1) + ".png")   
 
     return
-
-
-        # Get prediction for given input image 
-def get_prediction(s, img, all_nodes):
-
-    data = np.asarray(img_crop(img, IMG_PATCH_SIZE, IMG_PATCH_SIZE))
-    data_node = tf.constant(data)
- 
-    output = tf.nn.softmax(model(data_node, all_nodes))
-    output_prediction = s.run(output)
-    img_prediction = label_to_img(img.shape[0], img.shape[1], IMG_PATCH_SIZE, IMG_PATCH_SIZE, output_prediction)
-
-    return img_prediction
-
-
-# Get a concatenation of the prediction and groundtruth for given input file
-def get_prediction_with_groundtruth(img, img_prediction, image_idx):
-    #print("img_prediction", img_prediction)
-    cimg = concatenate_images(img, img_prediction)
-    return cimg
-
-
-
-# Get prediction overlaid on the original image for given input file
-def get_prediction_with_overlay(img, img_prediction, idx):
-    oimg = make_img_overlay(img, img_prediction) 
-    return oimg
---
+++ prediction.py
@@ -0,0 +1,43 @@
+import tensorflow as tf
+import tensorflow.keras as keras
+
+from ressource_files.mask_to_submission import *
+from helper import *
+from post_processing import *
+
+
+
+# Split 608 wide image into 4 smaller 400*400 images to be able to use the model
+# Input:    img         608*608 input image 
+#           model       Unet model     
+# Output    pred        reconstructed 608*608 image from 4 400*400 images    
+def img_predict(img, model):
+    """0 and 1 matrix prediction from an image"""
+    imgs = np.array([img[:400, :400], img[:400, -400:], img[-400:, :400], img[-400:, -400:]])
+
+    preds = model.predict(imgs)
+    pred = np.zeros((img.shape[0], img.shape[1], 1))
+    pred[:400, :400]    = preds[0]
+    pred[:400, -400:]   = preds[1]
+    pred[-400:, :400]   = preds[2]
+    pred[-400:, -400:]  = preds[3]
+    return pred
+
+
+def main(argv=None): 
+
+    img_test = [PATH_TEST_DATA + 'test_' + str(i+1 ) + '/' + 'test_' + str(i + 1) + '.png' for i in range(4)]
+    model = keras.models.load_model(PATH_model)
+
+    for idx, path in enumerate(img_test):    
+        print(idx)
+        img = np.squeeze(get_image(path))
+        pred = img_predict(img, model)
+        pred = np.squeeze(pred).round()
+
+        #generate prediction image, with overlay for for visual inspection
+        generate_output(idx, img, pred)
+
+
+if __name__ == '__main__':
+    tf.compat.v1.app.run()
