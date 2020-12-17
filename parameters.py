5607bcde037dd955c4e1f417e15b77d36cfe66d0
raph2d68 <raphael.ausilio@epfl.ch>
2020-12-17 18:07:42 +0100

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -2,10 +2,10 @@ NUM_CHANNELS = 3  # RGB images
 PIXEL_DEPTH = 255
 NUM_LABELS = 2
 TRAINING_SIZE = 10
-VALIDATION_SIZE = 10  # Size of the validation set.
+VALIDATION_SPLIT = 0.2  # Size of the validation set.
 SEED = 66478  # Set to None for random seed.
 BATCH_SIZE = 16  # 64
-NUM_EPOCHS = 20
+NUM_EPOCHS = 2
 RESTORE_MODEL = False  # If True, restore existing model instead of training a new one
 RECORDING_STEP = 0
 
@@ -14,7 +14,7 @@ RECORDING_STEP = 0
 # image size should be an integer multiple of this number!
 IMG_PATCH_SIZE = 20
 
-POST_PROCESS = 1
+POST_PROCESS = 0
 TOL_ANGLE = 30
 RAND_TRAIN = 0
 
@@ -24,12 +24,15 @@ NUM_FILTER = 32
 FILTER_SIZE = 3
 NUMBER_OF_KERNEL = 3
 
+
+FOLDER_PATH= '/content/gdrive/MyDrive/ML-project2-main/ML-project2-main/'
+
 #Path for training
-TRAIN_DIR =         '/content/gdrive/MyDrive/ML-project2-main/ML-project2-main/ressource_files/training/'
-PATH_UNET_MODEL =   '/content/gdrive/MyDrive/ML-project2-main/ML-project2-main/model_files/'
+TRAIN_DIR =         FOLDER_PATH +'ressource_files/training/'
+PATH_UNET_MODEL =   FOLDER_PATH +'model_files/'
 
 #Path for prediction 
-PATH_model =        PATH_UNET_MODEL +'unet.h5'
-PATH_TEST_DATA =    '/content/gdrive/MyDrive/ML-project2-main/ML-project2-main/ressource_files/test_set_images/'
-PATH_PREDICTION_DIR = '/content/gdrive/MyDrive/ML-project2-main/ML-project2-main/ressource_files/prediction/'
-PATH_SUBMISSION =   'final_submission.csv'
+PATH_model =          PATH_UNET_MODEL +'unet.h5'
+PATH_TEST_DATA =      FOLDER_PATH + 'ressource_files/test_set_images/'
+PATH_PREDICTION_DIR = FOLDER_PATH + 'ressource_files/prediction/'
+PATH_SUBMISSION =     FOLDER_PATH +' final_submission.csv'
