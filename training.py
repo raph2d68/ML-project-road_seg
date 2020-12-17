c0b721fa18b2d9409e8feb511a2a7779a2532a19
raph2d68 <raphael.ausilio@epfl.ch>
2020-12-17 16:13:26 +0100

    Update training.py

--- training.py
+++ training.py
@@ -14,14 +14,6 @@ def main(argv=None):
     chosen_img_names = get_data_names(train_image_dir, TRAINING_SIZE, RAND_TRAIN )
     imgs, grth_imgs = augment_data(train_image_dir, train_grth_dir, chosen_img_names)
     
-    print('imgs shape', imgs.shape)
-    print('grth shape', grth_imgs.shape)
-    #n = imgs.shape[0]
-    #m = grth_imgs.shape[0]
-    
-    #list_imgs = [imgs[i, :, :, :] for i in range(n)]
-    #list_grth_imgs = [grth_imgs[i, :, :] for i in range(m)]
-    
     x_train = np.asarray(imgs)
     y_train = np.expand_dims(np.asarray(grth_imgs), axis=3)
 
@@ -35,9 +27,6 @@ def main(argv=None):
     print('Saving trained model')
     model.save(PATH_UNET_MODEL)
     # plot_metric_history(f1_scores)
-
-    
-
 
 if __name__ == '__main__':
     tf.compat.v1.app.run()
